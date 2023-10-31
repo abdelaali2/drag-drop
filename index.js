7a19f83b366a51482aaa4dc7fb493eb37dec1f54
Ibrahim Badr <ibrahim.badreldin@spectro.net>
2023-10-31 14:44:09 +0200

    delete feature added

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ function createNewElement(ev, type) {
 
 function createCard(x, y, type) {
   const newCard = document.createElement("div");
+  const cardId = Math.random().toString();
+  newCard.id = cardId;
   newCard.classList.add("card", "text-center", "position-absolute");
   newCard.draggable = true;
   newCard.ondragend = (ev) => {
@@ -28,19 +30,30 @@ function createCard(x, y, type) {
 
   newCard.style.left = `${x}px`;
   newCard.style.top = `${y}px`;
-  newCard.style.minWidth = `250px`;
-  newCard.style.minHeight = `100px`;
 
-  newCard.appendChild(createCardHeader(type));
+  newCard.appendChild(createCardHeader(type, cardId));
   newCard.appendChild(createCardBody(type));
 
   return newCard;
 }
 
-function createCardHeader(title) {
+function createCardHeader(title, cardId) {
   const cardHeader = document.createElement("div");
-  cardHeader.classList.add("card-header");
-  cardHeader.innerText = title.toUpperCase();
+  cardHeader.classList.add("card-header", "d-flex", "justify-content-between");
+  cardHeader.style.cursor = "grab";
+
+  const cardTitle = document.createElement("h3");
+  cardTitle.innerText = title.toUpperCase();
+
+  const deleteBtn = document.createElement("h3");
+  deleteBtn.onclick = () => {
+    document.getElementById(cardId).remove();
+  };
+  deleteBtn.classList.add("bi", "bi-trash3-fill");
+
+  cardHeader.appendChild(cardTitle);
+  cardHeader.appendChild(deleteBtn);
+
   return cardHeader;
 }
 
