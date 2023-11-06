9e0b460e0dae8776fff65f5767ba920c914f12eb
Ibrahim Badr <hamedb.ibrahim@gmail.com>
2023-11-06 22:10:22 +0200

    linking feature added with state management

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ function createCard(x, y, type) {
   newCard.appendChild(createCardBody(type));
 
   const finalCard = attachControlBtns(newCard, cardId);
-
   finalCard.id = cardId;
   finalCard.classList.add(
     "position-absolute",
@@ -34,14 +33,14 @@ function createCard(x, y, type) {
     "flex-row",
     "align-items-center"
   );
+  finalCard.style.left = `${x}px`;
+  finalCard.style.top = `${y}px`;
+
   newCard.draggable = true;
   newCard.ondragend = (ev) => {
     finalCard.style.left = `${ev.clientX}px`;
     finalCard.style.top = `${ev.clientY}px`;
   };
-
-  finalCard.style.left = `${x}px`;
-  finalCard.style.top = `${y}px`;
   newCard.style.minHeight = `10rem`;
   newCard.style.minWidth = `12rem`;
   newCard.classList.add("card", "text-center", "mx-1");
@@ -75,7 +74,10 @@ function createCardBody(type) {
   cardBody.classList.add("card-body", "px-3");
   cardBody.contentEditable = true;
   cardBody.addEventListener("input", () => {
-    cardBody.dataset.input = cardBody.innerText || cardBody.value;
+    const currentText = cardBody.innerText || cardBody.value;
+    // Data processing
+    cardBody.dataset.input = currentText;
+    cardBody.dataset.output = currentText.toUpperCase();
   });
   return cardBody;
 }
@@ -85,13 +87,32 @@ function attachControlBtns(card) {
   const beforeControlBtn = document.createElement("button");
   beforeControlBtn.classList.add("ctrlBtn");
   beforeControlBtn.onclick = () => {
-    console.log(targetElem.dataset.input || "No Input Yet");
+    const input = linkingFeature.getData();
+    const coords = {
+      x: beforeControlBtn.offsetLeft,
+      y: beforeControlBtn.offsetTop,
+    };
+    if (input) {
+      targetElem.dataset.input = input;
+      targetElem.innerText = input;
+      targetElem.value = input;
+    }
+    linkingFeature.setPointB(coords);
+    createLinkingLine();
   };
 
   const afterControlBtn = document.createElement("button");
   afterControlBtn.classList.add("ctrlBtn");
   afterControlBtn.onclick = () => {
-    console.log(targetElem.dataset.output || "No Output Yet");
+    const output = targetElem.dataset.output;
+    const coords = {
+      x: afterControlBtn.offsetLeft,
+      y: afterControlBtn.offsetTop,
+    };
+    if (output) {
+      linkingFeature.setData(output);
+    }
+    linkingFeature.setPointA(coords);
   };
 
   const finalCard = document.createElement("div");
@@ -99,4 +120,55 @@ function attachControlBtns(card) {
   finalCard.appendChild(card);
   finalCard.appendChild(afterControlBtn);
   return finalCard;
+}
+
+// State Management
+const linkingFeature = {
+  data: null,
+  pointA: null,
+  pointB: null,
+  setData(data) {
+    this.data = data;
+  },
+  getData() {
+    return this.data;
+  },
+  setPointA(pointA) {
+    this.pointA = pointA;
+  },
+  getPointA() {
+    return this.pointA;
+  },
+  setPointB(pointB) {
+    this.pointB = pointB;
+  },
+  getPointB() {
+    return this.pointB;
+  },
+};
+
+function createLinkingLine() {
+  const line = document.createElement("div");
+  const point1 = linkingFeature.getPointA();
+  console.log("point1", point1);
+  const point2 = linkingFeature.getPointB();
+  console.log("point2", point2);
+
+  if (!Object.keys(point1).length || !Object.keys(point2).length) return;
+
+  const distance = Math.sqrt(
+    Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2)
+  );
+
+  const angle = Math.atan2(point2.y - point1.y, point2.x - point1.x);
+
+  line.style.position = "absolute";
+  line.style.zIndex = 10;
+  line.style.left = point1.x + "px";
+  line.style.top = point1.y + "px";
+  line.style.width = distance + "px";
+  line.style.transform = `rotate(${angle}rad)`;
+
+  line.style.border = "1px solid black";
+  BODY.appendChild(line);
 }
